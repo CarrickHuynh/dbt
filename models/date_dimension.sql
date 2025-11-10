e63cfa2218011f265877d7499d56e43e3bec4091
Huỳnh Danh <carrick.huynh@mainetti.com>
2025-11-10 17:30:16 +0000

    daily_weather model

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -6,21 +6,13 @@ date(to_timestamp(started_At)) as date_stated_At,
 hour(to_timestamp(started_At)) as hour_stated_At,
 
 
-case 
-when dayname(to_timestamp(started_At)) in ('Sat','Sun')
-then 'Weekend'
-else 'Ngày làm việc'
-end as day_type,
 
-case 
-when month(to_timestamp(started_At)) in (12,1,2)
-then 'Winter'
-when  month(to_timestamp(started_At)) in (3,4,5)
-then 'Srping'
-when  month(to_timestamp(started_At)) in (6,7,8)
-then 'Summer'
-else 'Autum'
-end as month_type
+{{day_type('started_At')}} as day_type,
+
+
+{{get_season('started_At')}} as season_type,
+
+
 from 
 {{source('demo','bike')}}
 
